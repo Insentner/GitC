a4dae4f40f64ac2d2e2940540722023c37dd351a
INSENTNER <INSENTNER@INSENTNER-PC>
2019-04-17 00:11:03 +0300

    Fix lab2 if > ? and head.h

--- Lab_2/src_lib/task2_f.c
+++ Lab_2/src_lib/task2_f.c
@@ -1,13 +1,4 @@
-#include <math.h>
-
 double f(double x)
 {
-	if (x < 3.2)
-	{
-		return pow(x, 4) + 9;
-	}
-	else
-	{
-		return 54 * pow(x, 4) / (-5 * pow(x, 2) + 7);
-	}
+	return (x < 3.2) ? pow(x, 4) + 9 : 54 * pow(x, 4) / (-5 * pow(x, 2) + 7);
 }
