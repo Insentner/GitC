2876a793d0d5a0bd7867fa48d0aae19257abbd60
darkIns <darkIns@DESKTOP-CS1CPR1>
2019-04-17 11:23:12 +0300

    FIXLAB

--- Lab_3/version for/includes/head.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <math.h>
-#include <stdio.h>
-
-double a(int i);
---
+++ Lab_4/Task1/src_exe/main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+
+void main(void)
+{
+	double X = 9;
+	double res;
+	const double *x = &X;
+	double *result = &res;
+
+	f(x, result);
+	printf("f(x)=%.4lf\n", result);
+	system("pause");
+}
