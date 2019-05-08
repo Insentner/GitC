f17f2f5e3b6133b0ddd34f09b0da7a01f6526505
darkIns <darkIns@DESKTOP-CS1CPR1>
2019-05-08 11:04:26 +0300

    prototype 5

---
+++ Lab_5/src_lib/find.c
@@ -0,0 +1,20 @@
+
+
+
+_Bool podstrahuy(const char *str, const char *substr)
+{
+	while ()
+	{
+
+	}
+}
+
+char *find(const char *str, const char *substr)
+{
+	if (podstrahuy(*str, *substr))
+	{
+
+	}
+	else
+
+}
---
+++ Lab_5/src_lib/strlen.c
@@ -0,0 +1,11 @@
+#include <head.h>
+
+int strlen(const char *str)
+{
+	int len = 0;
+	while (*(str + len) != '\0');
+	{
+		len++;
+	}
+	return len;
+}
