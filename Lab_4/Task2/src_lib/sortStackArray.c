c2d27044d0c0e330e26bceff369e776ceef9bde4
darkIns <darkIns@DESKTOP-CS1CPR1>
2019-05-08 09:56:48 +0300

    LAB4

--- Lab_4/Task2/src_lib/sortStackArray.c
+++ Lab_4/Task2/src_lib/sortStackArray.c
@@ -5,10 +5,8 @@ void sortStackArray()
 	int n;
 	int arr[5];
 	n = sizeof(arr) / sizeof(int);
-	printf("I\n");//check
+	printf("%d\n",n);
 	readArray(arr, n);
-	printf("II\n");
 	sort(arr, n);
-	printf("III\n");
 	writeArray(arr, n);
 }
