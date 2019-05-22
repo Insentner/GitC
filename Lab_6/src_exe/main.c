32264c97823bf12b6d71338a85b3f28e0c07141e
INSENTNER <INSENTNER@INSENTNER-PC>
2019-05-22 22:50:18 +0300

    OMG

--- Lab_6/src_exe/main.c
+++ Lab_6/src_exe/main.c
@@ -3,9 +3,9 @@
 #include <locale.h>
 #include <head.h>
 
-void write1(void)
+struct ZACH *write1(void)
 {
-	struct ZACH Z1 =
+	static struct ZACH Z1 =
 	{
 		"Вася",
 		"Машина",
@@ -40,7 +40,7 @@ void write1(void)
 					{ "", "", "..", { Diff, {.mark = Excel} }  },
 					{ "", "", "..", { Diff, {.mark = Excel} }  },
 					{ "", "", "..", { Diff, {.mark = Excel} }  },
-					{ "", "", "..", { Diff, {.mark = Excel} }  }
+					{ "", "", "..", { Diff, {.mark = Good} }  }
 				}
 			},
 			{ 2018,
@@ -68,6 +68,8 @@ void write1(void)
 					{ "", "", "..", { notDiff, {.mark = Excel} }  },
 					{ "", "", "..", { notDiff, {.acc = 1} }  },
 					{ "", "", "..", { notDiff, {.acc = 1} }  },
+					{ "", "", "..", { notDiff, {.acc = 1} }  },
+					{ "", "", "..", { notDiff, {.acc = 1} }  },
 					{ "", "", "..", { notDiff, {.acc = 1} }  }
 				},
 				{
@@ -79,25 +81,276 @@ void write1(void)
 			},
 		}
 	 };
+	return &Z1;
 }
-void write2(void)
+struct ZACH *write2(void)
 {
-	struct ZACH Z2;
+	static struct ZACH Z2;
 	strcpy(Z2.firstName, "Вася");
 	strcpy(Z2.lastName, "Машина");
+
+	//1й семестр
 	Z2.semI[0].sem = 1;
 	Z2.semI[0].year = 2017;
+
 	strcpy(Z2.semI[0].theor[0].name, "Введение в специальность");
 	strcpy(Z2.semI[0].theor[0].teach, "Гончаров");
 	strcpy(Z2.semI[0].theor[0].time, "22.12.17");
 	Z2.semI[0].theor[0].str.type = notDiff;
 	Z2.semI[0].theor[0].str.value.acc = 1;
 
+	strcpy(Z2.semI[0].theor[1].name, "История России");
+	strcpy(Z2.semI[0].theor[1].teach, "");
+	strcpy(Z2.semI[0].theor[1].time, "");
+	Z2.semI[0].theor[1].str.type = notDiff;
+	Z2.semI[0].theor[1].str.value.acc = 1;
 
+	strcpy(Z2.semI[0].theor[2].name, "");
+	strcpy(Z2.semI[0].theor[2].teach, "");
+	strcpy(Z2.semI[0].theor[2].time, "");
+	Z2.semI[0].theor[2].str.type = notDiff;
+	Z2.semI[0].theor[2].str.value.acc = 1;
+
+	strcpy(Z2.semI[0].theor[3].name, "");
+	strcpy(Z2.semI[0].theor[3].teach, "");
+	strcpy(Z2.semI[0].theor[3].time, "");
+	Z2.semI[0].theor[3].str.type = notDiff;
+	Z2.semI[0].theor[3].str.value.acc = 1;
+
+	strcpy(Z2.semI[0].theor[4].name, "");
+	strcpy(Z2.semI[0].theor[4].teach, "");
+	strcpy(Z2.semI[0].theor[4].time, "");
+	Z2.semI[0].theor[4].str.type = notDiff;
+	Z2.semI[0].theor[4].str.value.acc = 1;
+
+
+	strcpy(Z2.semI[0].prack[0].name, "");
+	strcpy(Z2.semI[0].prack[0].teach, "");
+	strcpy(Z2.semI[0].prack[0].time, "");
+	Z2.semI[0].prack[0].str.type = Diff;
+	Z2.semI[0].prack[0].str.value.mark = Good;
+
+	strcpy(Z2.semI[0].prack[1].name, "");
+	strcpy(Z2.semI[0].prack[1].teach, "");
+	strcpy(Z2.semI[0].prack[1].time, "");
+	Z2.semI[0].prack[1].str.type = Diff;
+	Z2.semI[0].prack[1].str.value.mark = Excel;
+
+	strcpy(Z2.semI[0].prack[2].name, "");
+	strcpy(Z2.semI[0].prack[2].teach, "");
+	strcpy(Z2.semI[0].prack[2].time, "");
+	Z2.semI[0].prack[2].str.type = Diff;
+	Z2.semI[0].prack[2].str.value.mark = Excel;
+
+	//2й семестр
+	Z2.semI[1].sem = 2;
+	Z2.semI[1].year = 2018;
+
+	strcpy(Z2.semI[1].theor[0].name, "");
+	strcpy(Z2.semI[1].theor[0].teach, "");
+	strcpy(Z2.semI[1].theor[0].time, "");
+	Z2.semI[1].theor[0].str.type = notDiff;
+	Z2.semI[1].theor[0].str.value.acc = 1;
+
+	strcpy(Z2.semI[1].theor[1].name, "История России");
+	strcpy(Z2.semI[1].theor[1].teach, "");
+	strcpy(Z2.semI[1].theor[1].time, "");
+	Z2.semI[1].theor[1].str.type = notDiff;
+	Z2.semI[1].theor[1].str.value.acc = 1;
+
+	strcpy(Z2.semI[1].theor[2].name, "");
+	strcpy(Z2.semI[1].theor[2].teach, "");
+	strcpy(Z2.semI[1].theor[2].time, "");
+	Z2.semI[1].theor[2].str.type = notDiff;
+	Z2.semI[1].theor[2].str.value.acc = 1;
+
+	strcpy(Z2.semI[1].theor[3].name, "");
+	strcpy(Z2.semI[1].theor[3].teach, "");
+	strcpy(Z2.semI[1].theor[3].time, "");
+	Z2.semI[1].theor[3].str.type = notDiff;
+	Z2.semI[1].theor[3].str.value.acc = 1;
+
+	strcpy(Z2.semI[1].theor[4].name, "");
+	strcpy(Z2.semI[1].theor[4].teach, "");
+	strcpy(Z2.semI[1].theor[4].time, "");
+	Z2.semI[1].theor[4].str.type = notDiff;
+	Z2.semI[1].theor[4].str.value.acc = 1;
+
+
+	strcpy(Z2.semI[1].prack[0].name, "");
+	strcpy(Z2.semI[1].prack[0].teach, "");
+	strcpy(Z2.semI[1].prack[0].time, "");
+	Z2.semI[1].prack[0].str.type = Diff;
+	Z2.semI[1].prack[0].str.value.mark = Excel;
+
+	strcpy(Z2.semI[1].prack[1].name, "");
+	strcpy(Z2.semI[1].prack[1].teach, "");
+	strcpy(Z2.semI[1].prack[1].time, "");
+	Z2.semI[1].prack[1].str.type = Diff;
+	Z2.semI[1].prack[1].str.value.mark = Excel;
+
+	strcpy(Z2.semI[1].prack[2].name, "");
+	strcpy(Z2.semI[1].prack[2].teach, "");
+	strcpy(Z2.semI[1].prack[2].time, "");
+	Z2.semI[1].prack[2].str.type = Diff;
+	Z2.semI[1].prack[2].str.value.mark = Excel;
+
+	strcpy(Z2.semI[1].prack[3].name, "");
+	strcpy(Z2.semI[1].prack[3].teach, "");
+	strcpy(Z2.semI[1].prack[3].time, "");
+	Z2.semI[1].prack[3].str.type = Diff;
+	Z2.semI[1].prack[3].str.value.mark = Good;
+
+	//3й семестр
+	Z2.semI[2].sem = 3;
+	Z2.semI[2].year = 2018;
+
+	strcpy(Z2.semI[2].theor[0].name, "");
+	strcpy(Z2.semI[2].theor[0].teach, "");
+	strcpy(Z2.semI[2].theor[0].time, "");
+	Z2.semI[2].theor[0].str.type = notDiff;
+	Z2.semI[2].theor[0].str.value.acc = 1;
+
+	strcpy(Z2.semI[2].theor[1].name, "");
+	strcpy(Z2.semI[2].theor[1].teach, "");
+	strcpy(Z2.semI[2].theor[1].time, "");
+	Z2.semI[2].theor[1].str.type = notDiff;
+	Z2.semI[2].theor[1].str.value.acc = 1;
+
+	strcpy(Z2.semI[2].theor[2].name, "");
+	strcpy(Z2.semI[2].theor[2].teach, "");
+	strcpy(Z2.semI[2].theor[2].time, "");
+	Z2.semI[2].theor[2].str.type = notDiff;
+	Z2.semI[2].theor[2].str.value.mark = Good;
+
+	strcpy(Z2.semI[2].theor[3].name, "");
+	strcpy(Z2.semI[2].theor[3].teach, "");
+	strcpy(Z2.semI[2].theor[3].time, "");
+	Z2.semI[2].theor[3].str.type = notDiff;
+	Z2.semI[2].theor[3].str.value.acc = 1;
+
+	strcpy(Z2.semI[2].theor[4].name, "");
+	strcpy(Z2.semI[2].theor[4].teach, "");
+	strcpy(Z2.semI[2].theor[4].time, "");
+	Z2.semI[2].theor[4].str.type = notDiff;
+	Z2.semI[2].theor[4].str.value.acc = 1;
+
+	strcpy(Z2.semI[2].theor[5].name, "");
+	strcpy(Z2.semI[2].theor[5].teach, "");
+	strcpy(Z2.semI[2].theor[5].time, "");
+	Z2.semI[2].theor[5].str.type = notDiff;
+	Z2.semI[2].theor[5].str.value.acc = 1;
+
+
+	strcpy(Z2.semI[2].prack[0].name, "");
+	strcpy(Z2.semI[2].prack[0].teach, "");
+	strcpy(Z2.semI[2].prack[0].time, "");
+	Z2.semI[2].prack[0].str.type = Diff;
+	Z2.semI[2].prack[0].str.value.mark = Excel;
+
+	strcpy(Z2.semI[2].prack[1].name, "");
+	strcpy(Z2.semI[2].prack[1].teach, "");
+	strcpy(Z2.semI[2].prack[1].time, "");
+	Z2.semI[2].prack[1].str.type = Diff;
+	Z2.semI[2].prack[1].str.value.mark = Excel;
+
+	strcpy(Z2.semI[2].prack[2].name, "");
+	strcpy(Z2.semI[2].prack[2].teach, "");
+	strcpy(Z2.semI[2].prack[2].time, "");
+	Z2.semI[2].prack[2].str.type = Diff;
+	Z2.semI[2].prack[2].str.value.mark = Excel;
+
+	strcpy(Z2.semI[2].prack[3].name, "");
+	strcpy(Z2.semI[2].prack[3].teach, "");
+	strcpy(Z2.semI[2].prack[3].time, "");
+	Z2.semI[2].prack[3].str.type = Diff;
+	Z2.semI[2].prack[3].str.value.mark = Excel;
+
+	//4й семестр
+	Z2.semI[3].sem = 4;
+	Z2.semI[3].year = 2019;
+
+	strcpy(Z2.semI[3].theor[0].name, "");
+	strcpy(Z2.semI[3].theor[0].teach, "");
+	strcpy(Z2.semI[3].theor[0].time, "");
+	Z2.semI[3].theor[0].str.type = notDiff;
+	Z2.semI[3].theor[0].str.value.acc = 1;
+
+	strcpy(Z2.semI[3].theor[1].name, "");
+	strcpy(Z2.semI[3].theor[1].teach, "");
+	strcpy(Z2.semI[3].theor[1].time, "");
+	Z2.semI[3].theor[1].str.type = notDiff;
+	Z2.semI[3].theor[1].str.value.acc = 1;
+
+	strcpy(Z2.semI[3].theor[2].name, "");
+	strcpy(Z2.semI[3].theor[2].teach, "");
+	strcpy(Z2.semI[3].theor[2].time, "");
+	Z2.semI[3].theor[2].str.type = notDiff;
+	Z2.semI[3].theor[2].str.value.mark = Excel;
+
+	strcpy(Z2.semI[3].theor[3].name, "");
+	strcpy(Z2.semI[3].theor[3].teach, "");
+	strcpy(Z2.semI[3].theor[3].time, "");
+	Z2.semI[3].theor[3].str.type = notDiff;
+	Z2.semI[3].theor[3].str.value.acc = 1;
+
+	strcpy(Z2.semI[3].theor[4].name, "");
+	strcpy(Z2.semI[3].theor[4].teach, "");
+	strcpy(Z2.semI[3].theor[4].time, "");
+	Z2.semI[3].theor[4].str.type = notDiff;
+	Z2.semI[3].theor[4].str.value.acc = 1;
+
+	strcpy(Z2.semI[3].theor[5].name, "");
+	strcpy(Z2.semI[3].theor[5].teach, "");
+	strcpy(Z2.semI[3].theor[5].time, "");
+	Z2.semI[3].theor[5].str.type = notDiff;
+	Z2.semI[3].theor[5].str.value.acc = 1;
+
+	strcpy(Z2.semI[3].theor[5].name, "");
+	strcpy(Z2.semI[3].theor[5].teach, "");
+	strcpy(Z2.semI[3].theor[5].time, "");
+	Z2.semI[3].theor[5].str.type = notDiff;
+	Z2.semI[3].theor[5].str.value.acc = 1;
+
+	strcpy(Z2.semI[3].theor[5].name, "");
+	strcpy(Z2.semI[3].theor[5].teach, "");
+	strcpy(Z2.semI[3].theor[5].time, "");
+	Z2.semI[3].theor[5].str.type = notDiff;
+	Z2.semI[3].theor[5].str.value.acc = 1;
+
+
+	strcpy(Z2.semI[3].prack[0].name, "");
+	strcpy(Z2.semI[3].prack[0].teach, "");
+	strcpy(Z2.semI[3].prack[0].time, "");
+	Z2.semI[3].prack[0].str.type = Diff;
+	Z2.semI[3].prack[0].str.value.mark = Excel;
+
+	strcpy(Z2.semI[3].prack[1].name, "");
+	strcpy(Z2.semI[3].prack[1].teach, "");
+	strcpy(Z2.semI[3].prack[1].time, "");
+	Z2.semI[3].prack[1].str.type = Diff;
+	Z2.semI[3].prack[1].str.value.mark = Excel;
+
+	strcpy(Z2.semI[3].prack[2].name, "");
+	strcpy(Z2.semI[3].prack[2].teach, "");
+	strcpy(Z2.semI[3].prack[2].time, "");
+	Z2.semI[3].prack[2].str.type = Diff;
+	Z2.semI[3].prack[2].str.value.mark = Excel;
+
+	strcpy(Z2.semI[3].prack[3].name, "");
+	strcpy(Z2.semI[3].prack[3].teach, "");
+	strcpy(Z2.semI[3].prack[3].time, "");
+	Z2.semI[3].prack[3].str.type = Diff;
+	Z2.semI[3].prack[3].str.value.mark = Excel;
+
+	return &Z2;
 }
+
 void main(void)
 {
 	setlocale(LC_ALL, "Rus");
+	struct ZACH *Z1 = write1();
+	struct ZACH *Z2 = write2();
 
 	
 }
