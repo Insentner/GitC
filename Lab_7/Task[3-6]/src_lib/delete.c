d0da00a7084a67c1ca3ef6a0a3a36401f98e907a
INSENTNER <Insentnser@gmail.com>
2019-05-29 12:13:32 +0300

    bad

--- Lab_7/Task[3-6]/src_lib/delete.c
+++ Lab_7/Task[3-6]/src_lib/delete.c
@@ -4,17 +4,19 @@ char *delete(char *str, const char *substr)
 {
 	int i = 0, j = 0;
 	const char *s = find(str, substr);
-	char *st = (char *)malloc(sizeof(str) + 1);
-	while (*(s + i) != '\0' && i < strlen(substr))
+	char *st = (char *)calloc(strlen(str), sizeof(char));
+	while (i < sizeof(st)/sizeof(char)  && i < s - str)
 	{
+		*(st + i) = *(str + i);
 		i++;
 	}
-	while (*(str + i + 1 + j) != '\0')
+	while (*(str + i + j + strlen(substr)) != '\0' /*&& i < strlen(substr)*/)
 	{
-		*(st + i + j) = *(str );
+		*(st + j + i) = *(str + i + j + strlen(substr));
 		j++;
 	}
-	str = (char *)realloc(str, sizeof(s) - i+1);
+	*str = "";
+	*str = (char **)realloc(*str, strlen(st) * sizeof(char)+1);
 
-	return &str;
+	return str;
 }
