e78110f7903f41feeea66c55f3c81de59aea2cfa
INSENTNER <Insentnser@gmail.com>
2019-06-05 00:56:56 +0300

    mini

--- Lab_5/src_lib/delete.c
+++ Lab_5/src_lib/delete.c
@@ -3,16 +3,20 @@
 void delete(char *str, const char *substr)
 {
 	int i = 0, j = 0;
-	str = find(str, substr);
-	while (*(str + i) != '\0' && i < strlen(substr))
+	char *s = find(str, substr);
+	if (s == NULL)
 	{
-        *(str + i) = '\0';
+		return "error: not found";
+	}
+	while (*(s + i) && i < strlen(substr))
+	{
+		*(s + i) = '\0';
 		i++;
 	}
-	while (*(str + j + i) != '\0')
+	while (*(s + j + i))
 	{
-		*(str + j) =*(str + j + i);
-		*(str + j + i) = '\0';
+		*(s + j) = *(s + j + i);
+		*(s + j + i) = '\0';
 		j++;
 	}
 }
